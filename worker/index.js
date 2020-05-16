33d900e592fef8f9a38828b8667ebebf2adf5c63
chtushar <choudharit23@gmail.com>
2020-05-16 20:28:54 +0530

    cron worker added and WORKING

--- worker/index.js
+++ worker/index.js
@@ -1,5 +1,10 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+var CronJob = require("cron").CronJob;
+let fetchGithub = require("./tasks/fetch-github");
+var job = new CronJob(
+  "*/1 * * * *",
+  fetchGithub,
+  null,
+  true,
+  "America/Los_Angeles"
+);
 job.start();
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,7 +1,31 @@
-let fetch = require('node-fetch');
+let fetch = require("node-fetch");
+const redis = require("redis");
+const client = redis.createClient();
 
-export default async function fetchGithub() {
-    const jobs = await fetch('https://jobs.github.com/positions.json?description=javascript');
+const { promisify } = require("util");
+// const getAsync = promisify(client.get).bind(client);
+//const setAsync = promisify(client.set).bind(client);
+
+let baseUrl = "https://jobs.github.com/positions.json?description=javascript";
+
+async function fetchGithub() {
+  let resultCount = 1,
+    onPage = 0;
+  let allJobs = [];
+
+  while (resultCount > 0) {
+    const res = await fetch(`${baseUrl}&page=${onPage}`);
+    const jobs = await res.json();
+    allJobs.push(...jobs);
+    resultCount = jobs.length;
+    console.log(jobs.length);
+    onPage++;
+  }
+
+  //const success = await setAsync("github", allJobs);
+  console.log(allJobs.length);
 }
 
 fetchGithub();
+
+module.exports = fetchGithub;
